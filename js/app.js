3de1b30db20fe439ed54ecd1c2e9f03ac1f3cbf4
willy93 <cordonwilly24@gmail.com>
2020-02-13 15:23:32 -0300

    4

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-/**
- * Procedemos a instalar el sw con el comando navigator.serviceWorker.register
- * 
- * Integramos el service worker de One signal a nuestro sw
- * 
- * 
- */
-var url = window.location.href;
-var swLocation = 'Pwa_Benefcios/sw.js';
-var swReg;
+// /**
+//  * Procedemos a instalar el sw con el comando navigator.serviceWorker.register
+//  * 
+//  * Integramos el service worker de One signal a nuestro sw
+//  * 
+//  * 
+//  */
+// var url = window.location.href;
+// var swLocation = '/Pwa_Benefcios/sw.js';
+// var swReg;
 
-let test=url.includes('localhost');
+// let test=url.includes('localhost');
 
-if ( navigator.serviceWorker ) {
+// if ( navigator.serviceWorker ) {
     
     
-    if ( url.includes('localhost') ) {
-        swLocation = 'Pwa_Benefcios/sw.js';
-    }
+//     if ( url.includes('localhost') ) {
+//         swLocation = '/Pwa_Benefcios/sw.js';
+//     }
 
 
-    window.addEventListener('load', function() {
-        console.log(swLocation);
-        navigator.serviceWorker.register( swLocation );
+//     window.addEventListener('load', function() {
+//         console.log(swLocation);
+//         navigator.serviceWorker.register( swLocation );
 
-    });
+//     });
 
-}
+// }
